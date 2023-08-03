9eab6f68f424bde4ee6869e679d4fb769a382566
PariaRF <paria.rezaeefard@gmail.com>
2023-08-03 15:24:01 +0330

    Add Title For Pages

--- client/js/pages/AboutUs.js
+++ client/js/pages/AboutUs.js
@@ -1,7 +1,8 @@
 const AboutUs = () => {
-    return `<h1 class="maintnance__container">
+    return `<div class="maintnance__container">
+    <h1>درباره ما</h1>
     <img src="client/assets/images/maintnance.png"/>
-    </h1>`;
+    </div>`;
 };
 
 export default AboutUs;
--- client/js/pages/Representation.js
+++ client/js/pages/Representation.js
@@ -1,5 +1,6 @@
 const Representation = () => {
     return `<div class="maintnance__container">
+    <h1>اعطای نمایندگی</h1>
     <img src="client/assets/images/maintnance.png"/>
     </div>`;
 };
